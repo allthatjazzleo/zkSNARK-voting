4f658ebcad853c5015b5e8d8d299622cdaf8ff88
allthatjazzleo <pangleo1994@gmail.com>
2022-04-03 17:42:14 +0800

    enforece whitelist verifier address

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -25,20 +25,31 @@ contract Election{
 
 	mapping(uint=>uint8) public votesReceived;
 	mapping(bytes32=>bool) public voted;
+	mapping(address=>bool) public verifierAddr;
 	event Voted(
 			uint indexed _candidateid
 	);
+	event whitelistedVerifierAddr(address indexed theaddr);
 	uint[] public candidateList=[1,2,3];
+
+	constructor(address[] memory _verifierAddr) public {
+    for (uint8 i = 0; i < _verifierAddr.length; i++){
+			verifierAddr[_verifierAddr[i]] = true;
+      emit whitelistedVerifierAddr(_verifierAddr[i]);
+		}
+	}
+
 
 	function totalVotesFor(uint candidate) view public returns (uint8){
 		require(validCandidate(candidate));
 		return votesReceived[candidate];
 	}
 
-	function voteForCandidate(address verifierAddr, uint candidate, IVerifier.Proof memory proof, uint[1] memory input) public {
+	function voteForCandidate(address _verifierAddr, uint candidate, IVerifier.Proof memory proof, uint[1] memory input) public {
 		bytes32 hash = keccak256(abi.encodePacked(proof.a.X, proof.a.Y, proof.b.X, proof.b.Y, proof.c.X, proof.c.Y, input));
 		require(voted[hash] == false);
-		require(IVerifier(verifierAddr).verifyTx(proof, input) == true);
+		require(verifierAddr[_verifierAddr] == true);
+		require(IVerifier(_verifierAddr).verifyTx(proof, input) == true);
 		require(validCandidate(candidate));
 		votesReceived[candidate] +=	1;
 		voted[hash] = true;
